c612170268d9a2889c1bf0d1aed1c9c8650435c9
Krunal Patel <krunalp2102@gmail.com>
2020-06-12 23:31:07 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) =>{
+    res.send({hi:'there'});
+})
+
+//Heroku can inject env port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
