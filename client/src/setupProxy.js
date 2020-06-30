167f2478e27178cf1d359dc609860ce9a20217ca
krunalpatel2194 <krunalp2102@gmail.com>
2020-06-30 15:24:37 -0400

    gitL clone

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,15 +1,9 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
-    app.use(
-        ["/api", "/auth/google"],
-        createProxyMiddleware({
-            target: 'http://localhost:9024',
-        })
-    );
-    app.use(
-        ["/auth"],
-        createProxyMiddleware({
-            target: 'http://localhost:9024',
-        })
-    );
+  app.use(
+    ['/api', '/auth/google'],
+    createProxyMiddleware({
+      target: 'http://localhost:9024',
+    })
+  );
 };
