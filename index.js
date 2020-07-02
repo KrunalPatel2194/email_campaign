12b6e2d2fce4c594fa336500310850a9be13e3c2
krunalpatel2194 <krunalp2102@gmail.com>
2020-07-02 02:56:09 -0400

    Stripe payment added

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport'); 
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -19,7 +21,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
-
+require('./routes/billingRoutes')(app);
 //Heroku can inject env port
 const PORT = process.env.PORT || 9024;
 app.listen(PORT); 
