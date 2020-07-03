b0d2bb72e876018ca95b685ddb6ef57abbdc2ff1
krunalpatel2194 <krunalp2102@gmail.com>
2020-07-02 22:30:26 -0400

    production fix

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ require('./routes/billingRoutes')(app);
 if(process.env.NODE_ENV === 'production'){
     // Express will serve up production assets
     //like our main.js or main.css file !
+    console.log("in prod");
     app.use(express.static('client/build'));
     // Express will serve up the index.html file
     // if it doesn't recognize the route
